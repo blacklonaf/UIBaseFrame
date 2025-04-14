e7ec8fb80188dfc2733bf107523e7362fa16d1af
blacklonaf <blacklonaf@gmail.com>
2025-04-15 00:10:18 +0900

    ok

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Map App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
+      ),
+      home: MainPage()
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Flexible(
+            child: Container(
+              width: double.infinity, height: double.infinity,
+              child: Text("MapWidget"),
+
+            ),
+            flex: 5,
+          ),
+          Flexible(
+              child: Column(
+                children: [
+
+                ],
+              )
+          ),
+        ],
+      ),
+    );
+  }
+}
